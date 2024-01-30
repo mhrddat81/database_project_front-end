c82db598248b35e7ae408c0263f39abe0bc9bd81
mhrddat81 <97848045+mhrddat81@users.noreply.github.com>
2024-01-30 11:08:46 +0330

    refactoring login and login_script to the version before the flask

--- static/login_script.js
+++ static/login_script.js
@@ -1,34 +1,19 @@
 function login() {
+    // Simple login logic
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Send login credentials to the server for authentication
-    fetch('/authenticate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            // If authentication is successful, redirect to the dashboard
-            window.location.href = '/dashboard';
-        } else {
-            alert('Invalid username or password. Please try again.');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    // You can replace this with your actual authentication logic
+    if (username === 'demo' && password === 'password') {
+        window.location.href = 'dashboard.html';
+    } else if (username === null || password === null) {
+        alert('Invalid username or password. Please try again.');
+    }
+    
 }
 
 function goToForgotPassword() {
-    window.location.href = 'forgot_password.html';
+    window.location.href = 'forgot-password.html';
 }
 
 function sendRecoveryEmail() {
@@ -47,40 +32,3 @@ function register() {
     // Add logic to handle user registration
     alert('User registered!\nUsername: ' + newUsername + '\nPassword: ' + newPassword);
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    var recoveryForm = document.getElementById('recoveryForm');
-
-    recoveryForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        var email = document.getElementById('email').value;
-
-        // Add logic to send recovery email
-        sendRecoveryEmail(email);
-    });
-
-    function sendRecoveryEmail(email) {
-        // Add AJAX or fetch request to send recovery email
-        fetch('/process_recovery', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                alert('Password recovery email sent to ' + email);
-            } else {
-                alert('Failed to send recovery email. Please try again.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }
-});
